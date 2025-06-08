2f2556534462fbf645443bc5fc79d4991065b78f
Andy Gonzalez <gonzalezandy21@gmail.com>
2025-06-08 01:39:14 -0400

    Initial Commit

---
+++ api/Models/Comment.cs
@@ -0,0 +1,20 @@
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations.Schema;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace api.Models
+{
+    public class Comment
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = "";
+        public string Content { get; set; } = "";
+        public DateTime CreatedOn { get; set; } = DateTime.Now;
+
+        public int? StockId { get; set; }
+        [ForeignKey("StockId")]
+        public Stock? Stock { get; set; }
+    }
+}
